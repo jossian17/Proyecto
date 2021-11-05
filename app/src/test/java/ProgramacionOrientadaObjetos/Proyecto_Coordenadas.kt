3ab644a83b6097583d775242a32e8003e07fba94
jossian17 <93722133+jossian17@users.noreply.github.com>
2021-11-05 16:09:13 +0100

    Update Proyecto_Coordenadas.kt

--- app/src/test/java/ProgramacionOrientadaObjetos/Proyecto_Coordenadas.kt
+++ app/src/test/java/ProgramacionOrientadaObjetos/Proyecto_Coordenadas.kt
@@ -72,5 +72,20 @@ open class Camino() {
         }
         return rumboFinal
     }
-}
 
+    fun comenzarCamino(): Array<Coordenada> {
+        val definirEscala = Escalar_Mapa(Mapa.VIZCAYA, Mapa.ARIZGOITI)
+        definirEscala.Calcular()
+        val puntos: Array<Coordenada> = arrayOf(
+            Coordenada(43.45367, -2.75221, "Cabo Machichaco"),
+            Coordenada(43.31336, -2.67962, "Casa de Juntas Gernika"),
+            Coordenada(43.26614, -2.93799, "Museo de Bellas Artes de Bilbao"),
+            Coordenada(43.30148, -3.04201, "Funicular de Larreineta"),
+            Coordenada(43.25329, -3.13091, "Torre Loizaga"),
+            Coordenada(43.19879, -2.83914, "Embalse Lekubaso"),
+            Coordenada(43.17481, -2.81030, "Argiñatxe Kobak"),
+            Coordenada(42.98596, -2.83197, "Parque Natural Gorbeia")
+        )
+        return puntos
+    }
+}
